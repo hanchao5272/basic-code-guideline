2752328bd46378918d6377a3b108f048189cf957
hanchao <hanchao@yidian-inc.com>
2018-06-28 17:17:58 +0800

    项目初始建立

---
+++ src/main/java/pers/hanchao/basiccodeguideline/BasicCodeGuidelineApplication.java
@@ -0,0 +1,12 @@
+package pers.hanchao.basiccodeguideline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCodeGuidelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicCodeGuidelineApplication.class, args);
+    }
+}
