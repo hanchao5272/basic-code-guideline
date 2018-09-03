ae09528ff6c2bd8f94c48250187c4461993e070f
hanchao <hanchao@yidian-inc.com>
2018-09-03 18:05:38 +0800

    通过异或实现 0 和 1 互转

---
+++ src/main/java/pers/hanchao/basiccodeguideline/bit/ReverseDemo.java
@@ -0,0 +1,15 @@
+package pers.hanchao.basiccodeguideline.bit;
+
+/**
+ * <p>Description: 通过异或实现 0 和 1 互转 </P>
+ *
+ * @author hanchao
+ * @date 2018/8/14 下午8:06
+ */
+public class ReverseDemo {
+    public static void main(String[] args) {
+
+        System.out.println(1 ^ 0);
+        System.out.println(1 ^ 1);
+    }
+}
